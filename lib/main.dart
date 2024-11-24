986c8612c9bfb476e949b22ad6d3a16236012910
WanderJ08 <wanderroque01@gmail.com>
2024-11-24 18:56:56 -0400

    Ci: Adding permissions to gallery, microfone and camera

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:tiktok_clone/authentication/authentication_controller.dart';
 import 'package:tiktok_clone/authentication/login_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp().then((value) {
+    Get.put(AuthenticationController());
+  });
+
   runApp(const MyApp());
 }
 
