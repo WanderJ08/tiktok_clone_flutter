f7aa6bd2e6feb8416b2eb8570593ad275fbc2998
WanderJ08 <wanderroque01@gmail.com>
2024-11-27 14:22:51 -0400

    Feat: Add videos and thumbnail images to firesabe storage function

--- lib/home/upload_video/upload_controller.dart
+++ lib/home/upload_video/upload_controller.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:get/get.dart';
 import 'package:video_compress/video_compress.dart';
 
@@ -6,12 +7,41 @@ class UploadController extends GetxController {
     final compressVideoFilePath = await VideoCompress.compressVideo(
         videoFilePath,
         quality: VideoQuality.MediumQuality);
-
     return compressVideoFilePath!.file;
+  }
+
+  uploadCompressedVideoFiletoFirebaseStorage(
+      String videoID, String videoFilePath) async {
+    UploadTask videoUploadTask = FirebaseStorage.instance
+        .ref()
+        .child("All Videos")
+        .child(videoID)
+        .putFile(await compressVideoFile(videoFilePath));
+
+    TaskSnapshot snapshot = await videoUploadTask;
+
+    String dowloadUrlOfUploadedVideo = await snapshot.ref.getDownloadURL();
+
+    return dowloadUrlOfUploadedVideo;
   }
 
   getThumbnailImage(String videoFilePath) async {
     final thumbnailImage = await VideoCompress.getFileThumbnail(videoFilePath);
     return thumbnailImage;
+  }
+
+  uploadThumbnailImagetoFirebaseStorage(
+      String videoID, String videoFilePath) async {
+    UploadTask thumbnailUploadTask = FirebaseStorage.instance
+        .ref()
+        .child("All Thumbnails")
+        .child(videoID)
+        .putFile(await getThumbnailImage(videoFilePath));
+
+    TaskSnapshot snapshot = await thumbnailUploadTask;
+
+    String dowloadUrlOfUploadedThumbnail = await snapshot.ref.getDownloadURL();
+
+    return dowloadUrlOfUploadedThumbnail;
   }
 }
