0034ad47db17a00b650e8b809c6d38e671afa6d3
WanderJ08 <wanderroque01@gmail.com>
2024-11-27 10:19:47 -0400

    Feat: Display video on Upload form function added

--- lib/home/upload_video/upload_form.dart
+++ lib/home/upload_video/upload_form.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 class UploadForm extends StatefulWidget {
   final File videoFile;
@@ -13,8 +14,44 @@ class UploadForm extends StatefulWidget {
 }
 
 class _UploadFormState extends State<UploadForm> {
+  VideoPlayerController? playerController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+
+    setState(() {
+      playerController = VideoPlayerController.file(widget.videoFile);
+    });
+
+    playerController!.initialize();
+    playerController!.play();
+    playerController!.setVolume(2);
+    playerController!.setLooping(true);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    playerController!.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            //Display video player
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 1.3,
+              child: VideoPlayer(playerController!),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
