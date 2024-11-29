a76bd5712f35f6f0742a6b0042bc35387f8cb1c6
WanderJ08 <wanderroque01@gmail.com>
2024-11-28 21:40:00 -0400

    Feat: Circular profile image animation

---
+++ lib/widgets/circular_image_animation.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class CircularImageAnimation extends StatefulWidget {
+  final Widget? widgetAnimation;
+
+  CircularImageAnimation({this.widgetAnimation});
+
+  @override
+  State<CircularImageAnimation> createState() => _CircularImageAnimationState();
+}
+
+class _CircularImageAnimationState extends State<CircularImageAnimation>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controllerAnimation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+
+    controllerAnimation = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 5000),
+    );
+    controllerAnimation.forward();
+    controllerAnimation.repeat();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+
+    controllerAnimation.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RotationTransition(
+      turns: Tween(
+        begin: 0.0,
+        end: 1.0,
+      ).animate(controllerAnimation),
+      child: widget.widgetAnimation,
+    );
+  }
+}
